28faa7a9aaddd65668b6612ad9109992adab96ed
Noé Godin <noegodin443@gmail.com>
2025-09-22 13:09:52 +0200

    git init

---
+++ src/main/java/com/utc/worlder/WorlderApplication.java
@@ -0,0 +1,13 @@
+package com.utc.worlder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorlderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorlderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utc/worlder/WorlderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.worlder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorlderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
